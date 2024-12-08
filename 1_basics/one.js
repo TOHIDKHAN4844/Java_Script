54049c14cbc0c0908272c14a701d1111597cb59d
Tohid Khan <120051579+TOHIDKHAN4844@users.noreply.github.com>
2024-12-08 08:34:59 +0000

    DataTypes

--- 1_basics/one.js
+++ 1_basics/one.js
@@ -9,3 +9,11 @@ console.table([rollNo,emailId,city])
 console.log(typeof null) // object
 console.log(typeof undefined) // undefined
 
+console.log(2**4) // 2^4
+
+console.log(1+3+"3")//43
+console.log("1"+1+3)//113
+
+// Primitive dataType string,number, bool, null,undefined BigInt,symbol
+
+//Refrence (non Primitive ) -> Arrays, Objects, Functions

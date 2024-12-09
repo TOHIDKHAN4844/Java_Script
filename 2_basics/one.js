bfc43812b5b1b6943e89edaf0a423881d94ead40
Tohid Khan <120051579+TOHIDKHAN4844@users.noreply.github.com>
2024-12-09 19:11:06 +0000

    arrays-Oprations

--- 2_basics/one.js
+++ 2_basics/one.js
@@ -33,3 +33,18 @@ console.log("C ", arr);
 console.log(myn2);
 
 
+const arr1=['one','two', 'three', 'four']
+const arr2=['five','six','seven'];
+const merge=[...arr1,...arr2]
+
+console.log(merge);
+
+console.log(Array.isArray("TohidKhan"))
+console.log(Array.from("TohidKhan"))
+console.log(Array.from({name:"TohidKhan"}))// interesting
+
+
+scorel=100
+score2 =200
+score3 = 300
+console.log (Array.of (scorel,score2,score3));

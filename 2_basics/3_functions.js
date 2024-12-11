de50eaa1e29877a1d0d7f1ea23b05a34115f861e
Tohid Khan <120051579+TOHIDKHAN4844@users.noreply.github.com>
2024-12-11 19:49:55 +0000

    MoreDetailsAboutFunctions

--- 2_basics/3_functions.js
+++ 2_basics/3_functions.js
@@ -1,8 +1,10 @@
+
+const ans=addNumbers(3,5);
+
 function addNumbers(no1,no2){
     console.log(no1+no2)
     return no1+no2;
 }
-const ans=addNumbers(3,5);
 console.log(ans);
 
 function calculateCartProce(val1,val2,...num){
@@ -19,3 +21,30 @@ function handelObject(anyobj){
 }
 
 handelObject(JsUser);
+
+const addTwoNo=function(no1,no2){
+    console.log(no1+no2);
+}
+addTwoNo(2,4)
+
+const user={
+    userName:"Tohid",
+    RollNo:150,
+
+    greeting: function(){
+        console.log(`${this.userName},welcome to Website.`);
+        console.log(this);
+    }
+}
+user.greeting();
+user.userName="Khan"
+user.greeting();
+
+
+const addNo=(no1,no2)=>{
+    return no1+no2
+}
+console.log(addNo(3,5));
+
+const noAdd=(no1,no2)=>no1+no2
+console.log(noAdd(3,9));
